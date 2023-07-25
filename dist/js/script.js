4afa00207968212988fa521b43cc5d701ad6ba8c
ridho008 <ciuyy12@gmail.com>
2023-07-25 20:53:46 +0700

    dark mode, fix fiture mobile and web

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,58 @@
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener('click', function() {
-   hamburger.classList.toggle('hamburger-active');
-   navMenu.classList.toggle('hidden');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // navbar fixed
-window.onscroll = function() {
-   const header = document.querySelector('header');
-   const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const toTop = document.querySelector("#toTop");
+  const fixedNav = header.offsetTop;
 
-   if(window.pageYOffset > fixedNav) {
-      header.classList.add('navbar-fixed');
-   } else {
-      header.classList.remove('navbar-fixed');
-   }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+// hidden hamburger when click anything
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  // darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Change button toggle sesuai dengan mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }

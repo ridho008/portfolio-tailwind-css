176eae360c0ee5c37b514ba96f4c7b7d22012683
ridho008 <ciuyy12@gmail.com>
2024-08-09 16:41:04 +0700

    updated all fitures as contact me, add design portfolio and etc

--- dist/js/script.js
+++ dist/js/script.js
@@ -56,3 +56,55 @@ if (
 } else {
   darkToggle.checked = false;
 }
+
+// Modal Portfolio
+// Get all the img elements in the grid
+var images = document.querySelectorAll(".grid img");
+
+// Loop through each img element
+images.forEach(function (img) {
+  // Add a click event listener to each img element
+  img.addEventListener("click", function () {
+    showModal(img.src);
+  });
+});
+
+// Get the modal by id
+var modal = document.getElementById("modal");
+
+// Get the modal image tag
+var modalImg = document.getElementById("modal-img");
+
+// This function is called when a small image is clicked
+function showModal(src) {
+  modal.classList.remove("hidden");
+  modalImg.src = src;
+}
+
+// This function is called when the close button is clicked
+function closeModal() {
+  modal.classList.add("hidden");
+}
+
+// contact form to google sheets
+// https://github.com/jamiewilson/form-to-google-sheets?tab=readme-ov-file
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzEgDbFEMrN0iZ6hQExRxehPIDCeqUxJhdUxDosF072toRCb_U3xHdM5JGf9yyXVvt1/exec";
+const form = document.forms["submit-to-google-sheet"];
+const alertSuccess = document.querySelector(".my-alert");
+const btnSend = document.querySelector(".btn-send");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnSend.setAttribute("disabled", "");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      alertSuccess.classList.toggle("hidden");
+      // disabled button send
+      btnSend.removeAttribute("disabled");
+      // reset input form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
